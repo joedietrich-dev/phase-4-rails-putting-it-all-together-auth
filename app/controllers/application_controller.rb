5c14d81876ea5264d3df055e5004988321f180a2
joedietrich-dev <joe@joedietrich.net>
2022-01-01 22:11:46 -0500

    commit tree 659051e493f5e843950d8917b2da8f1913733d27

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid_errors
+
+  before_action :authorize
+
+  private
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: { errors: ['Invalid username or password'] }, status: :unauthorized unless @current_user
+  end
+
+  def render_record_invalid_errors(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
